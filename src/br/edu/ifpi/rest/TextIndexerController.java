9a6c95e1a881836ce4df4ddb3a5caf16cfac206a
Ismaías <ismaias.13@hotmail.com>
2011-09-20 11:05:42 -0300

    commit

--- src/br/edu/ifpi/rest/TextIndexerController.java
+++ src/br/edu/ifpi/rest/TextIndexerController.java
@@ -1,20 +1,12 @@
 package br.edu.ifpi.rest;
 
 import static br.com.caelum.vraptor.view.Results.*;
-import static org.junit.Assert.assertEquals;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
-
 import org.apache.lucene.index.CorruptIndexException;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
-
-import com.google.gson.Gson;
-import com.thoughtworks.xstream.XStream;
-import com.thoughtworks.xstream.io.xml.DomDriver;
 
 import br.com.caelum.vraptor.Path;
 import br.com.caelum.vraptor.Resource;
@@ -45,13 +37,11 @@ public class TextIndexerController {
 	public ReturnMessage addText(MetaDocument metaDocument, String content) {
 
 		File path = new File("./webapps/indice/text");
-		//File path = new File(br.edu.ifpi.opala.utils.Path.TEXT_INDEX.getValue());
 		Directory indexDir;
 		ReturnMessage message = null;
 		if (metaDocument != null && content != null) {
 			try {
 				indexDir = FSDirectory.open(path);
-				//Directory indexDir = new RAMDirectory();
 				indexManager = new IndexManager(indexDir);
 				TextIndexer indexer = new NearRealTimeTextIndexer(indexManager);
 				message = indexer.addText(metaDocument, content);
@@ -70,31 +60,17 @@ public class TextIndexerController {
 		} else {
 			message = ReturnMessage.PARAMETER_INVALID;
 		}
-		/*
-			HashMap<Integer, String> mensagem = new HashMap<Integer, String>();  
-		 
-			mensagem.put(message.getCode(), message.getMessage());
-			
-			Map<String, Object>  mensagem2 = new HashMap<String, Object>();
-			mensagem2.put("code", message.getCode());
-			mensagem2.put("message", message.getMessage().toString());
-			XStream xStream = new XStream(new DomDriver());
-            xStream.alias("person", ResultMessage.class);
-            System.out.println(xStream.toXML(person));
-            */
 		result.use(json()).withoutRoot().from(message).serialize();
 		return message;
 	}
 	@Path("/delText")
 	public ReturnMessage delText(String id) throws IOException, InterruptedException {
 		File path = new File("./webapps/indice/text");
-		//File path = new File(br.edu.ifpi.opala.utils.Path.TEXT_INDEX.getValue());
 		Directory indexDir;
 		ReturnMessage message = null;
 		if (id != null) {
 			try {
 				indexDir = FSDirectory.open(path);
-				//Directory indexDir = new RAMDirectory();
 				indexManager = new IndexManager(indexDir);
 				TextIndexer indexer = new NearRealTimeTextIndexer(indexManager);
 				message = indexer.delText(id);
--- src/br/edu/ifpi/rest/TextSearcherController.java
+++ src/br/edu/ifpi/rest/TextSearcherController.java
@@ -1,13 +1,8 @@
 package br.edu.ifpi.rest;
 
-import static br.com.caelum.vraptor.view.Results.xml;
-import static br.com.caelum.vraptor.view.Results.json;
-
 import java.io.File;
 import java.io.IOException;
 import java.lang.reflect.Type;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -25,7 +20,6 @@ import br.edu.ifpi.opala.searching.NearRealTimeTextSearcher;
 import br.edu.ifpi.opala.searching.SearchResult;
 import br.edu.ifpi.opala.searching.TextSearcher;
 import br.edu.ifpi.opala.utils.IndexManager;
-import br.edu.ifpi.opala.utils.Metadata;
 
 @Resource
 public class TextSearcherController {
@@ -46,28 +40,14 @@ public class TextSearcherController {
 		Gson gson = new Gson();
 		Type type = new TypeToken<Map<String, String>>(){}.getType();
         Map<String, String> map = gson.fromJson(fields, type);
-		/*
-		if (fields!=null){
-			result.use(xml()).from(fields).serialize();
-			return null;
-		}*/
 	
 		File path = new File("./webapps/indice/text");
-		// File path = new
-		// File(br.edu.ifpi.opala.utils.Path.TEXT_INDEX.getValue());
 		Directory indexDir;
 		SearchResult resultado = null;
 
 		try {
 			indexDir = FSDirectory.open(path);
-			// Directory indexDir = new RAMDirectory();
 			indexManager = new IndexManager(indexDir);
-
-			//Map<String, String> fields = new HashMap<String, String>();
-			//fields.put(Metadata.CONTENT.getValue(), "palavra");
-
-			//List<String> returnedFields = newRetunedFieldsWithAuthorAndTitle();
-
 			TextSearcher searcher = new NearRealTimeTextSearcher(indexManager);
 
 			resultado = searcher.search(map, returnedFields, batchStart, batchSize,
@@ -83,31 +63,9 @@ public class TextSearcherController {
 				e.printStackTrace();
 			}
 		}
-		/*
-		 * if (resultado==null){
-		 * resultado.setCodigo(ReturnMessage.EMPTY_SEARCHER); }
-		 */
-		/*
-		 * System.out.println(resultado.getCodigo());
-		 * System.out.println(resultado.getItems().size());
-		 * System.out.println(resultado.getItems().iterator().next()
-		 * .getField(Metadata.AUTHOR.getValue()));
-		 * System.out.println(resultado.getItems().iterator().next()
-		 * .getField(Metadata.TITLE.getValue()));
-		 * System.out.println(resultado.getItems().iterator().next()
-		 * .getField(Metadata.KEYWORDS.getValue()));
-		 */
+		
 		String response = new Gson().toJson(resultado);
 		result.include("variable", response);
-		//result.use(json()).from(resultado).include("items","items.fields").serialize();
-		//result.use(json()).withoutRoot().from(response).serialize();
 		return resultado;
-	}
-
-	private static List<String> newRetunedFieldsWithAuthorAndTitle() {
-		List<String> returnedFields = new ArrayList<String>();
-		returnedFields.add(Metadata.AUTHOR.getValue());
-		returnedFields.add(Metadata.TITLE.getValue());
-		return returnedFields;
 	}
 }
